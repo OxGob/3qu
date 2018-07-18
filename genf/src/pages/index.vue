cd10e475250905a0e8722f3d296afee2bc29def2
oxgob <arvingo@yahoo.co.uk>
2018-07-18 13:28:51 +0100

    Fixed Splice

--- genf/src/pages/index.vue
+++ genf/src/pages/index.vue
@@ -6,17 +6,21 @@
       <!-- QDes Tab -->
         <q-tab-pane name="QDes">
           <q-card class="bg-cyan-2 q-ma-xl">
+            <q-card-title>Form Designer - New Model
+              <span slot="subtitle">Design the form. Create the questions. Shape the flow with follow-up questions by ordering as desired.</span>
+            </q-card-title>
+            <q-card-separator class="q-mb-md q-mt-xl"/>
             <q-card-main>
             <div v-for="(form, fIndex) in forms" :key="form.id">
-              <q-field class="q-mb-sm" label="Form Title: " helper="Please enter the title of the question.">
-                <q-input v-model="form.name" type="text" clearable />
+              <q-field class="q-mb-sm" label="Form Title: " helper="Please enter the title of the form.">
+                <q-input v-model="form.qname" type="text" clearable />
               </q-field>
               <q-card-separator class="q-mb-md q-mt-xl"/>
            <div v-for="(question, qIndex) in form.questions" :key="question.id">
             <q-btn class="q-mb-md" round size="sm" color="amber" icon="add" @click="addRowQuestions(fIndex)" />
             <q-btn class="vertical-top" v-show="qIndex !==0" round size="sm" color="blue" icon="remove" @click="remRowQs(fIndex)" />
             <q-field class="q-mb-sm" label="Question Title: " >
-              <q-input v-model="question.text" />
+              <q-input v-model="question.qtext" />
             </q-field>
             <q-card-separator class="q-mb-md q-mt-xl"/>
               <div v-for="(answerChoice, aIndex) in question.answerChoices" :key="answerChoice.id">
@@ -46,10 +50,10 @@ export default {
     return {
       forms: [
         {
-          name: '',
+          fname: '',
           questions: [
             {
-              text: '',
+              qtext: '',
               answerChoices: [
                 {
                   answerId: ''
@@ -64,7 +68,7 @@ export default {
   methods: {
     addRowQuestions (fIndex) {
       this.forms[fIndex].questions.push({
-        text: '',
+        qtext: '',
         answerChoices: [
           {
             answerId: ''
@@ -78,7 +82,7 @@ export default {
       })
     },
     remRowQs (fIndex) {
-      this.forms[fIndex].questions.answerChoices.splice(fIndex, 1)
+      this.forms[fIndex].questions.splice(fIndex, 1)
     },
     remRowAns (fIndex, qIndex) {
       this.forms[fIndex].questions[qIndex].answerChoices.splice(qIndex, 1)
